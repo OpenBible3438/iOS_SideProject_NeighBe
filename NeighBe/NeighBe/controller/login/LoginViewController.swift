78d49f4fda0f2df18dafb7b54851d7c150d09177
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-04-06 21:54:08 +0900

    extension 디렉토리 생성

---
+++ NeighBe/NeighBe/controller/login/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  NeighBe
+//
+//  Created by 편성경 on 2022/04/06.
+//
+
+import UIKit
+
+/*
+ 로그인
+ 
+ ##### to-do #####
+ - 화면 구현
+ - 로그인 버튼 화면 이동
+ - 회원가입 버튼 화면 이동
+ - id, pw 찾기
+ 
+ -소셜 로그인
+ */
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ NeighBe/NeighBe/extenstion/common/CommonColor.swift
@@ -0,0 +1,15 @@
+//
+//  CommonColor.swift
+//  NeighBe
+//
+//  Created by 편성경 on 2022/04/06.
+//
+
+import Foundation
+import UIKit
+
+class CommonColor {
+    
+    /* Main Color */
+    let appMainColor: UIColor = UIColor(red: 0.004, green: 0.118, blue: 0.220, alpha: 1.000)
+}
