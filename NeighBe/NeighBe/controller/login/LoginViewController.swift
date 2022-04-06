9072cdf1e8fb7a386bc1bf99ef9b9110b2ab0627
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-04-06 23:11:31 +0900

    Login 화면 구성

--- NeighBe/NeighBe/controller/login/LoginViewController.swift
+++ NeighBe/NeighBe/controller/login/LoginViewController.swift
@@ -11,18 +11,51 @@ import UIKit
  로그인
  
  ##### to-do #####
- - 화면 구현
+ - 화면 구현 O
  - 로그인 버튼 화면 이동
  - 회원가입 버튼 화면 이동
  - id, pw 찾기
  
- -소셜 로그인
+ - 소셜 로그인
+ - 자동 로그인 체크박스
  */
+
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var mainLogoImage: UIImageView!
+    @IBOutlet weak var appNameLabel: UILabel!
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var findUserButton: UIButton!
+    @IBOutlet weak var joinUserButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 버튼 텍스트 크기 조정
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        findUserButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        joinUserButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
 
     }
+    
+    /* Login Button Action */
+    @IBAction func userLoginAction(_ sender: Any) {
+        
+    }
+    
+    /* ID PW 찾기 Action */
+    @IBAction func findUserIDPWAction(_ sender: Any) {
+        
+    }
+    
+    /* 회원가입 Action */
+    @IBAction func joinUserAction(_ sender: Any) {
+        
+    }
+    
 
 }
